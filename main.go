f90a7189abc92c7047286fc1c4bc8b7084d3f481
tonymmm1 <tony@mail.magnatox.com>
2022-01-27 00:46:21 -0600

    added basic JSON storage of REST body

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
         "crypto/hmac"
         "crypto/sha256"
         "encoding/base64"
+        "encoding/json"
         "fmt"
         "os"
         "strconv"
@@ -18,6 +19,16 @@ type tomlConfig struct { //configuration toml file struct
     Key string
     Password string
     Secret string
+}
+
+type apiAccount struct { //array to store API account struct
+    Id string `json:"id"`
+    Currency string `json:"currency"`
+    Balance string `json:"balance"`
+    Hold string `json:"hold"`
+    Available string `json:"available"`
+    Profile_id string `json:"profile_id"`
+    Trading_enabled bool `json:"trading_enabled"`
 }
 
 func gen_api_message(api_key_password string, api_key_secret string, time_current string, request_method string, request_path string) string{
@@ -38,7 +49,9 @@ func gen_api_message(api_key_password string, api_key_secret string, time_curren
 
 func rest_handler(api_host string, api_key string, api_key_password string, api_key_secret string, request_method string, request_path string){
     time_current := strconv.FormatInt(time.Now().Unix(), 10)    //store current Unix time as int
+
     message_hashed := gen_api_message(api_key_password, api_key_secret, time_current, request_method, request_path) //create hashed message to send
+
     //REST client
     client := resty.New()
     resp, err := client.R().
@@ -62,6 +75,28 @@ func rest_handler(api_host string, api_key string, api_key_password string, api_
     fmt.Println("  Received At:", resp.ReceivedAt())
     fmt.Println("  Body       :\n", resp)
     fmt.Println()
+
+    //store REST Body as struct
+    var api_accounts []apiAccount
+    if err := json.Unmarshal(resp.Body(), &api_accounts); err != nil { //JSON unmarshal REST response body to store as struct
+        fmt.Println("ERROR decoding REST response")
+        os.Exit(1)
+    }
+
+    //debug
+    fmt.Println("api_accounts:")
+    fmt.Println()
+    for api_account := range api_accounts {
+        fmt.Printf("api_accounts[%d]:\n", api_account)
+        fmt.Println(api_accounts[api_account].Id)
+        fmt.Println(api_accounts[api_account].Currency)
+        fmt.Println(api_accounts[api_account].Balance)
+        fmt.Println(api_accounts[api_account].Hold)
+        fmt.Println(api_accounts[api_account].Available)
+        fmt.Println(api_accounts[api_account].Profile_id)
+        fmt.Println(api_accounts[api_account].Trading_enabled)
+        fmt.Println()
+    }
 }
 
 func main() {
