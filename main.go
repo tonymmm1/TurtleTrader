174a62fb923f7fac97ed160230368fa7ff9a32d3
tonymmm1 <tony@mail.magnatox.com>
2022-01-28 12:17:04 -0600

    added enum with common return codes

--- main.go
+++ main.go
@@ -16,6 +16,16 @@ import (
         "github.com/go-resty/resty/v2"
 )
 
+//Common return codes (https://docs.cloud.coinbase.com/exchange/docs/requests)
+const (
+    STATUS_CODE_SUCCESS int = 200           //Success
+    STATUS_CODE_BAD_REQUEST = 400,          //Bad Request -- Invalid request format
+    STATUS_CODE_UNAUTHORIZED = 401,         //Unauthorized -- Invalid API Key
+    STATUS_CODE_FORBIDDEN = 403,            //Forbidden -- You do not have access to the requested resource
+    STATUS_CODE_NOT_FOUND = 404             //Not Found
+    STATUS_CODE_INTERNAL_SERVER_ERROR = 500 //Internal Server Error -- We had a problem with our server
+)
+
 type tomlConfig struct { //configuration toml file struct
     Host string 
     Key string
