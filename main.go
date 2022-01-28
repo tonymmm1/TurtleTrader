03f0713223b0c7bd40df645c08c32180d4d6d228
tonymmm1 <tony@mail.magnatox.com>
2022-01-28 14:59:33 -0600

    added basic REST handler and specific functions

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-        "bufio"
         "crypto/hmac"
         "crypto/sha256"
         "encoding/base64"
-        "encoding/json"
+        //"encoding/json"
         "fmt"
         "os"
         "strconv"
-        "strings"
         "time"
 
         "github.com/BurntSushi/toml"
@@ -58,25 +56,23 @@ func gen_api_message(api_key_password string, api_key_secret string, time_curren
     return base64.StdEncoding.EncodeToString(hash.Sum(nil)) //return hashed message
 }
 
-func rest_handler(api_host string, api_key string, api_key_password string, api_key_secret string, request_method string, request_path string) {
+func rest_client_get(api_host string, api_key string, api_key_password string, api_key_secret string, request_path string) (int, []byte) { //handles GET requests
+    request_method := "GET"
     time_current := strconv.FormatInt(time.Now().Unix(), 10)    //store current Unix time as int
 
     message_hashed := gen_api_message(api_key_password, api_key_secret, time_current, request_method, request_path) //create hashed message to send
 
-    //REST client
     client := resty.New() //create REST session
-    client.SetDebug(true)
     resp, err := client.R().
-        SetHeader("Accept", "application/json"). 
+        SetHeader("Accept", "application/json").
         SetHeaders(map[string] string {
-            "CB-ACCESS-KEY" : api_key, 
-            "CB-ACCESS-SIGN" : message_hashed, 
-            "CB-ACCESS-TIMESTAMP" : time_current, 
-            "CB-ACCESS-PASSPHRASE" : api_key_password, 
+            "CB-ACCESS-KEY" : api_key,
+            "CB-ACCESS-SIGN" : message_hashed,
+            "CB-ACCESS-TIMESTAMP" : time_current,
+            "CB-ACCESS-PASSPHRASE" : api_key_password,
             "Content-Type" : "application/json"}).
         SetAuthToken(api_key).
         Get(api_host + request_path)
-
     // debug
     fmt.Println("Response Info:")
     fmt.Println("  Error      :", err)
@@ -88,33 +84,34 @@ func rest_handler(api_host string, api_key string, api_key_password string, api_
     fmt.Println("  Body       :\n", resp)
     fmt.Println()
 
-    if resp.StatusCode() != STATUS_CODE_SUCCESS { //check if API request was sucessful before proceeding
-        fmt.Println("ERROR API request returned " + "\"" + resp.Status() + "\"")
+    return resp.StatusCode(), resp.Body()
+}
+
+/*
+func rest_client_put() { //handles PUT requests
+}
+
+func rest_client_post() { //handles POST requests
+}
+
+func rest_client_delete() { //handles DELETE requests
+}
+*/
+
+func get_all_accounts(api_host string, api_key string, api_key_password string, api_key_secret string) int { //handles GET requests
+    request_path := "/accounts"
+
+    resp,_ := rest_client_get(api_host, api_key, api_key_password, api_key_secret, request_path)
+    if resp != STATUS_CODE_SUCCESS {
+        fmt.Println("ERROR REST GET status code: ", resp)
         os.Exit(1)
     }
 
-    //store REST Body as struct
-    var api_accounts []apiAccount
-    if err := json.Unmarshal(resp.Body(), &api_accounts); err != nil { //JSON unmarshal REST response body to store as struct
-        fmt.Println(err)
-        fmt.Println("ERROR decoding REST response")
-        os.Exit(1)
-    }
+    return resp
+}
 
-    //debug
-    fmt.Println("api_accounts:")
-    fmt.Println()
-    for api_account := range api_accounts {
-        fmt.Printf("api_accounts[%d]:\n", api_account)
-        fmt.Println(api_accounts[api_account].Id)
-        fmt.Println(api_accounts[api_account].Currency)
-        fmt.Println(api_accounts[api_account].Balance)
-        fmt.Println(api_accounts[api_account].Hold)
-        fmt.Println(api_accounts[api_account].Available)
-        fmt.Println(api_accounts[api_account].Profile_id)
-        fmt.Println(api_accounts[api_account].Trading_enabled)
-        fmt.Println()
-    }
+func rest_handler(api_host string, api_key string, api_key_password string, api_key_secret string) {
+    get_all_accounts(api_host, api_key, api_key_password, api_key_secret)
 }
 
 func main() {
@@ -136,18 +133,5 @@ func main() {
     api_key_password := config.Password
     api_key_secret := config.Secret
 
-    request_method := "GET"
-
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print(api_host + "/")
-    request_path, err := reader.ReadString('\n') 
-    if err != nil {
-        fmt.Println("ERROR reading input")
-        os.Exit(1)
-    }
-
-    request_path = "/" + request_path //append / to request_path
-    request_path = strings.TrimRight(request_path, "\r\n") //trim last chars from input
-
-    rest_handler(api_host, api_key, api_key_password, api_key_secret, request_method, request_path)
+    rest_handler(api_host, api_key, api_key_password, api_key_secret)
 }
