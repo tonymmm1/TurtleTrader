a4eae667e515ec9590c3dd01fb54687bc721a624
tonymmm1 <tony@mail.magnatox.com>
2022-01-28 15:13:56 -0600

    added JSON struct unmarshall back

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
         "crypto/hmac"
         "crypto/sha256"
         "encoding/base64"
-        //"encoding/json"
+        "encoding/json"
         "fmt"
         "os"
         "strconv"
@@ -41,7 +41,7 @@ type apiAccount struct { //array to store API account struct
     Trading_enabled bool `json:"trading_enabled"`
 }
 
-func gen_api_message(api_key_password string, api_key_secret string, time_current string, request_method string, request_path string) string {
+func gen_api_message(api_key_password string, api_key_secret string, time_current string, request_method string, request_path string) string { //generate hashed message for REST requests
     message := time_current + request_method + request_path //construct prehase message
 
     decoded_secret, err := base64.StdEncoding.DecodeString(api_key_secret) //decode base64 encoded api secret
@@ -98,16 +98,38 @@ func rest_client_delete() { //handles DELETE requests
 }
 */
 
-func get_all_accounts(api_host string, api_key string, api_key_password string, api_key_secret string) int { //handles GET requests
+func get_all_accounts(api_host string, api_key string, api_key_password string, api_key_secret string) []apiAccount { //handles GET requests
     request_path := "/accounts"
 
-    resp,_ := rest_client_get(api_host, api_key, api_key_password, api_key_secret, request_path)
-    if resp != STATUS_CODE_SUCCESS {
-        fmt.Println("ERROR REST GET status code: ", resp)
+    var api_accounts []apiAccount
+
+    response_status, response_body := rest_client_get(api_host, api_key, api_key_password, api_key_secret, request_path)
+    if response_status != STATUS_CODE_SUCCESS {
+        fmt.Println("ERROR REST GET status code: ", response_status)
         os.Exit(1)
     }
 
-    return resp
+    if err := json.Unmarshal(response_body, &api_accounts); err != nil { //JSON unmarshal REST response body to store as struct
+        fmt.Println("ERROR decoding REST response")
+        os.Exit(1)
+    }
+
+    //debug
+    fmt.Println("api_accounts:")
+    fmt.Println()
+    for api_account := range api_accounts {
+        fmt.Printf("api_accounts[%d]:\n", api_account)
+        fmt.Println(api_accounts[api_account].Id)
+        fmt.Println(api_accounts[api_account].Currency)
+        fmt.Println(api_accounts[api_account].Balance)
+        fmt.Println(api_accounts[api_account].Hold)
+        fmt.Println(api_accounts[api_account].Available)
+        fmt.Println(api_accounts[api_account].Profile_id)
+        fmt.Println(api_accounts[api_account].Trading_enabled)
+        fmt.Println()
+    }
+
+    return api_accounts
 }
 
 func rest_handler(api_host string, api_key string, api_key_password string, api_key_secret string) {
