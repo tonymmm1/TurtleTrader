79a0d78cb3a9921572692efa5e26284fe2a9a29d
tonymmm1 <tony@mail.magnatox.com>
2022-02-03 23:56:02 -0600

    added Get profiles and some minor clean ups

--- main.go
+++ main.go
@@ -226,6 +226,42 @@ type apiTransfer struct {
     Subtotal string `json:"subtotal"`
 }
 
+type reqFill struct {
+    Request_path string
+    Order_id string
+    Product_id string
+    Profile_id string
+    Limit int64
+    Before int64
+    After int64
+}
+
+type apiFill struct {
+    Trade_id int32 `json:"id"`
+    Product_id string `json:"product_id"`
+    Order_id string `json:"order_id"`
+    User_id string `json:"user_id"`
+    Profile_id string `json:"profile_id"`
+    Liquidity string `json:"liquidity"`
+    Price string `json:"price"`
+    Size string `json:"size"`
+    Fee string `json:"fee"`
+    Created_at string `json:"created_at"`
+    Side string `json:"side"`
+    Settled bool `json:"settled"`
+    Usd_volume string `json:"usd_volume"`
+}
+
+type apiProfile struct {
+    Id string `json:"id"`
+    User_id string `json:"user_id"`
+    Name string `json:"name"`
+    Active bool `json:"active"`
+    Is_default bool `json:"is_default"`
+    Has_margin bool `json:"has_margin"`
+    Created_at string `json:"created_at"`
+}
+
 func gen_api_message(api_key_secret string, time_current string, request_method string, request_path string) string { //generate hashed message for REST requests
     message := time_current + request_method + request_path //construct prehase message
 
@@ -257,6 +293,81 @@ func rest_get(api_struct apiConfig, request_path string) (int, []byte) { //handl
             "CB-ACCESS-TIMESTAMP" : time_current,
             "CB-ACCESS-PASSPHRASE" : api_struct.Password,
             "Content-Type" : "application/json"}).
+        SetAuthToken(api_struct.Key).
+        Get(api_struct.Host + request_path)
+
+    // debug
+    fmt.Println("Response Info:")
+    fmt.Println("  Error      :", err)
+    fmt.Println("  Status Code:", resp.StatusCode())
+    fmt.Println("  Status     :", resp.Status())
+    fmt.Println("  Proto      :", resp.Proto())
+    fmt.Println("  Time       :", resp.Time())
+    fmt.Println("  Received At:", resp.ReceivedAt())
+    fmt.Println("  Body       :\n", resp)
+    fmt.Println()
+
+    return resp.StatusCode(), resp.Body()
+}
+
+func rest_get_all_fills(api_struct apiConfig, request_struct reqFill) (int, []byte) { //handles GET requests
+    request_method := "GET"
+
+    time_current := strconv.FormatInt(time.Now().Unix(), 10)    //store current Unix time as int
+
+    message_hashed := gen_api_message(api_struct.Secret, time_current, request_method, request_struct.Request_path) //create hashed message to send
+
+    client := resty.New() //create REST session
+    resp, err := client.R().
+        SetHeader("Accept", "application/json").
+        SetHeaders(map[string] string {
+            "CB-ACCESS-KEY" : api_struct.Key,
+            "CB-ACCESS-SIGN" : message_hashed,
+            "CB-ACCESS-TIMESTAMP" : time_current,
+            "CB-ACCESS-PASSPHRASE" : api_struct.Password,
+            "Content-Type" : "application/json"}).
+        SetQueryParams(map[string] string {
+            "order_id" : request_struct.Order_id,
+            "product_id" : request_struct.Product_id,
+            "profile_id" : request_struct.Profile_id,
+            "limit" : strconv.FormatInt(request_struct.Limit, 10),
+            "before" : strconv.FormatInt(request_struct.Before, 10),
+            "after" : strconv.FormatInt(request_struct.After, 10)}).
+        SetAuthToken(api_struct.Key).
+        Get(api_struct.Host + request_struct.Request_path)
+
+    // debug
+    fmt.Println("Response Info:")
+    fmt.Println("  Error      :", err)
+    fmt.Println("  Status Code:", resp.StatusCode())
+    fmt.Println("  Status     :", resp.Status())
+    fmt.Println("  Proto      :", resp.Proto())
+    fmt.Println("  Time       :", resp.Time())
+    fmt.Println("  Received At:", resp.ReceivedAt())
+    fmt.Println("  Body       :\n", resp)
+    fmt.Println()
+
+    return resp.StatusCode(), resp.Body()
+}
+
+func rest_get_profiles(api_struct apiConfig, request_path string, active bool) (int, []byte) {
+    request_method := "GET"
+
+    time_current := strconv.FormatInt(time.Now().Unix(), 10)    //store current Unix time as int
+
+    message_hashed := gen_api_message(api_struct.Secret, time_current, request_method, request_path) //create hashed message to send
+
+    client := resty.New() //create REST session
+    resp, err := client.R().
+        SetHeader("Accept", "application/json").
+        SetHeaders(map[string] string {
+            "CB-ACCESS-KEY" : api_struct.Key,
+            "CB-ACCESS-SIGN" : message_hashed,
+            "CB-ACCESS-TIMESTAMP" : time_current,
+            "CB-ACCESS-PASSPHRASE" : api_struct.Password,
+            "Content-Type" : "application/json"}).
+        SetQueryParams(map[string] string {
+            "active" : strconv.FormatBool(active)}).
         SetAuthToken(api_struct.Key).
         Get(api_struct.Host + request_path)
 
@@ -743,17 +854,17 @@ func generate_crypto_address(api_struct apiConfig, api_account_id string) apiCry
     return api_account_address
 }
 
-func convert_currency(api_struct apiConfig, request_profile_id string, request_from string, request_to string, request_amount string) apiConvert { //Converts funs from currency to currency
+func convert_currency(api_struct apiConfig, profile_id string, from string, to string, amount string) apiConvert { //Converts funs from currency to currency
     request_path := "/conversions"
 
     var request_struct reqConvert
     var api_account_convert apiConvert
 
     request_struct.Request_path = request_path
-    request_struct.Profile_id = request_profile_id
-    request_struct.From = request_from
-    request_struct.To = request_to
-    request_struct.Amount = request_amount
+    request_struct.Profile_id = profile_id
+    request_struct.From = from
+    request_struct.To = to
+    request_struct.Amount = amount
 
     response_status, response_body := rest_post_convert_currency(api_struct, request_struct)
     if response_status != STATUS_CODE_SUCCESS {
@@ -819,6 +930,76 @@ func get_fees(api_struct apiConfig) apiFee {
     fmt.Println()
 
     return api_account_fees
+}
+
+func get_all_fills(api_struct apiConfig, order_id string, product_id string, profile_id string, limit int64, before int64, after int64) []apiFill {
+    request_path := "/fills"
+
+    var api_account_fills []apiFill
+
+    response_status, response_body := rest_get(api_struct, request_path)
+    if response_status != STATUS_CODE_SUCCESS {
+        fmt.Println("ERROR REST GET status code: ", response_status)
+        os.Exit(1)
+    }
+
+    if err := json.Unmarshal(response_body, &api_account_fills); err != nil { //JSON unmarshal REST response body to store as struct
+        fmt.Println("ERROR decoding REST response")
+        os.Exit(1)
+    }
+
+    //debug
+    fmt.Println("api_account_fills:")
+    fmt.Println()
+    for fill := range api_account_fills {
+        fmt.Println("api_account_fills[", fill, "]")
+        fmt.Println(api_account_fills[fill].Trade_id)
+        fmt.Println(api_account_fills[fill].Product_id)
+        fmt.Println(api_account_fills[fill].Order_id)
+        fmt.Println(api_account_fills[fill].User_id)
+        fmt.Println(api_account_fills[fill].Profile_id)
+        fmt.Println(api_account_fills[fill].Liquidity)
+        fmt.Println(api_account_fills[fill].Price)
+        fmt.Println(api_account_fills[fill].Size)
+        fmt.Println(api_account_fills[fill].Fee)
+    }
+    fmt.Println()
+
+    return api_account_fills
+}
+
+func get_profiles(api_struct apiConfig, active bool) []apiProfile{
+    request_path := "/profiles"
+
+    var profiles []apiProfile
+
+    response_status, response_body := rest_get(api_struct, request_path)
+    if response_status != STATUS_CODE_SUCCESS {
+        fmt.Println("ERROR REST GET status code: ", response_status)
+        os.Exit(1)
+    }
+
+    if err := json.Unmarshal(response_body, &profiles); err != nil { //JSON unmarshal REST response body to store as struct
+        fmt.Println("ERROR decoding REST response")
+        os.Exit(1)
+    }
+
+    //debug
+    fmt.Println("api_account_profiles:")
+    fmt.Println()
+    for profile := range profiles {
+        fmt.Println("profiles[", profile, "]")
+        fmt.Println(profiles[profile].Id)
+        fmt.Println(profiles[profile].User_id)
+        fmt.Println(profiles[profile].Name)
+        fmt.Println(profiles[profile].Active)
+        fmt.Println(profiles[profile].Is_default)
+        fmt.Println(profiles[profile].Has_margin)
+        fmt.Println(profiles[profile].Created_at)
+    }
+    fmt.Println()
+
+    return profiles
 }
 
 func rest_handler(api_struct apiConfig) {
