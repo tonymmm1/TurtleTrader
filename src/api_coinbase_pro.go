a107fe21e140b94ab351297005686fdccfa22513
tonymmm1 <tony@mail.magnatox.com>
2022-02-09 00:37:11 -0600

    added Get a currency

--- src/api_coinbase_pro.go
+++ src/api_coinbase_pro.go
@@ -165,10 +165,10 @@ type cbpCurrency struct { //Get all known currency/Get a currency
         Push_payment_methods []string `json:"push_payment_methods"`
         Group_types []string `json:"group_types"`
         Display_name string `json:"display_name"`
-        Processing_time_seconds float32 `json:"processing_time_seconds"`
+        Processing_time_seconds float64 `json:"processing_time_seconds"`
         Min_withdrawl_amount float64 `json:"min_withdrawl_amount"`
         Max_withdrawl_amount float64 `json:"max_withdrawl_amount"`
-    }`json:"details"`
+    } `json:"details"`
 }
 
 type cbpConvert struct { //Convert Currency/Get a conversion
@@ -596,6 +596,59 @@ func cbp_get_all_currencies() []cbpCurrency {
     }
 
     return currencies
+}
+
+func cbp_get_currency(currency_id string) cbpCurrency {
+    path := "/currencies/" + currency_id
+
+    var currency cbpCurrency
+
+    response_status, response_body := cbp_rest_get(path)
+    if response_status != CBP_STATUS_CODE_SUCCESS {
+        fmt.Println("ERROR REST GET status code: ", response_status)
+        os.Exit(1)
+    }
+
+    if err := json.Unmarshal(response_body, &currency); err != nil { //JSON unmarshal REST response body to store as struct
+        fmt.Println("ERROR decoding REST response")
+        os.Exit(1)
+    }
+
+    //debug
+    fmt.Println("Get a currency")
+    fmt.Println()
+    fmt.Println("currency:", currency_id)
+    fmt.Println(currency.Id)
+    fmt.Println(currency.Name)
+    fmt.Println(currency.Min_size)
+    fmt.Println(currency.Status)
+    fmt.Println(currency.Message)
+    fmt.Println(currency.Max_precision)
+    for convert := range currency.Convertible_to {
+        fmt.Println("currency.Convertible_to[", convert, "]")
+        fmt.Println(currency.Convertible_to[convert])
+    }
+    fmt.Println(currency.Details.Type)
+    fmt.Println(currency.Details.Symbol)
+    fmt.Println(currency.Details.Network_confirmations)
+    fmt.Println(currency.Details.Sort_order)
+    fmt.Println(currency.Details.Crypto_address_link)
+    fmt.Println(currency.Details.Crypto_transaction_link)
+    for payment := range currency.Details.Push_payment_methods {
+        fmt.Println("currency.Details.Push_payment_methods[", payment, "]")
+        fmt.Println(currency.Details.Push_payment_methods[payment])
+    }
+    for group := range currency.Details.Group_types {
+        fmt.Println("currency.Details.Group_types[", group, "]")
+        fmt.Println(currency.Details.Group_types[group])
+    }
+    fmt.Println(currency.Details.Display_name)
+    fmt.Println(currency.Details.Processing_time_seconds)
+    fmt.Println(currency.Details.Min_withdrawl_amount)
+    fmt.Println(currency.Details.Max_withdrawl_amount)
+    fmt.Println()
+
+    return currency
 }
 
 func cbp_convert_currency(profile_id string, from string, to string, amount string, nonce string) cbpConvert { //Converts funs from currency to currency
