64fc3fa60ea20edecf9b55d2b5abf0353dc8fcc0
tonymmm1 <tony@mail.magnatox.com>
2022-02-17 23:28:06 -0600

    fixed typoes for Coinbase return codes

--- src/api/coinbase_pro/api.go
+++ src/api/coinbase_pro/api.go
@@ -2,12 +2,12 @@
 package coinbase_pro
 
 const ( //Common return codes (https://docs.cloud.coinbase.com/exchange/docs/requests)
-    _STATUS_CODE_SUCCESS int = 200           //Success
-    _STATUS_CODE_BAD_REQUEST = 400           //Bad Request -- Invalid request format
-    _STATUS_CODE_UNAUTHORIZED = 401          //Unauthorized -- Invalid API Key
-    _STATUS_CODE_FORBIDDEN = 403             //Forbidden -- You do not have access to the requested resource
-    _STATUS_CODE_NOT_FOUND = 404             //Not Found
-    _STATUS_CODE_INTERNAL_SERVER_ERROR = 500 //Internal Server Error -- We had a problem with our server
+    STATUS_CODE_SUCCESS int = 200           //Success
+    STATUS_CODE_BAD_REQUEST = 400           //Bad Request -- Invalid request format
+    STATUS_CODE_UNAUTHORIZED = 401          //Unauthorized -- Invalid API Key
+    STATUS_CODE_FORBIDDEN = 403             //Forbidden -- You do not have access to the requested resource
+    STATUS_CODE_NOT_FOUND = 404             //Not Found
+    STATUS_CODE_INTERNAL_SERVER_ERROR = 500 //Internal Server Error -- We had a problem with our server
 )
 
 type Config struct { //Coinbase Pro configuration
